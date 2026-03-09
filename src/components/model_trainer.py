f784665da26f9b158211c02ee377e376d6e339e5
Ashish Behal <behay002@mymail.unisa.edu.au>
2026-03-09 15:04:06 +1030

    Added model evaluation with f1_score and roc_auc_score

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -10,6 +10,38 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from src.exception import CustomException
 from src.logger import logging
 
-from src.utils import save_object
+from src.utils import save_object,evaluate_models
 
 
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join('artifacts', 'model.pkl')
+
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config= ModelTrainerConfig
+
+    def initiate_model_trainer(self, train_arr, test_arr, preprocessor_path):
+        try:
+            logging.info("Splitting trainnig and test input data")
+            X_train, y_train, X_test, y_test= (
+                train_arr[:, :-1], # picks all rows and columns except last 
+                train_arr[:,-1], # picks all rows and only the last column
+                test_arr[:,:-1], # picks all rows and columns except last 
+                test_arr[:,-1] # picks all rows and only the last column
+            )
+
+            models = {
+                "LogisticRegression" : LogisticRegression(),
+                "RandomForestClassifier" : RandomForestClassifier(),
+                "GradientBoostingClassifier" : GradientBoostingClassifier(),
+            }
+
+            model_report: dict= evaluate_models(X=X_train,
+                                                y= y_train,
+                                                x_test= X_test,
+                                                y_test= y_test,
+                                                models=models )
+        except:
+            pass
--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import dill
 from src.exception import CustomException
-
+from sklearn.metrics import f1_score, roc_auc_score
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -15,3 +15,31 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)   
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models ):
+    report = {} #dictionary for storing each model's score
+    try:
+        for i in range(len(list(models))): #looping over every model in model dict.
+            models = list(models.values())[i] # picks the actual model object at ith index
+
+            models.fit(X_train, y_train) # Training the model
+
+            y_train_pred = models.predict(X_train) # makes predictions on training data
+
+            y_test_pred = models.predict(X_test) # make predictions on test data
+
+            train_f1= f1_score(y_train,y_train_pred)# f1: balances between precision and recall on training data
+            train_roc_auc_score= roc_auc_score(y_train,y_train_pred) # auc: how well model separates fraud vs non-fraud on training data
+                    
+            test_f1 = f1_score(y_test, y_test_pred) # f1: balances between precision and recall on test data
+            test_roc_auc_score = roc_auc_score(y_test, y_test_pred) # auc: how well model separates fraud vs non-fraud on test data
+
+            report[list(models.keys())[i]] = { # saving the model's name and its testscore into the report dict.
+                "f1 " : test_f1,
+                "roc_auc " :test_roc_auc_score }
+            
+        return report 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
